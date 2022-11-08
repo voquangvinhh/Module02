ec90fe4dbc0532c7eedbacc842537771961611a7
quangvinh18 <voquangvinh.dhqb@gmail.com>
2022-11-08 09:50:00 +0700

    add DisplayTypes.java file

---
+++ src/DisplayTypes.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class DisplayTypes {
+    public static void main(String[] args) {
+        int choice = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1. Print the rectangle");
+        System.out.println("2. Print the square triangle");
+        System.out.println("3. Print isosceles triangle");
+        System.out.println("4. Exit");
+        choice = sc.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Draw the triangle");
+                for (int i = 1; i <= 3; i++){
+                    for ( int j = 1; j <= 6; j++){
+                        System.out.print("*" + " ");
+                    }
+                    System.out.println("*" + "");
+                }
+                break;
+            case 2:
+                System.out.println("Print the square triangle");
+                for (int i =1; i <= 5; i++ ){
+                    for (int j =1; j < i; j++){
+                        System.out.print("*" + " ");
+                    }
+                    System.out.println("*" + " ");
+                }
+                break;
+            case 3:
+                System.out.println("Print isosceles triangle");
+                for (int i = 4; i >= 0; i--){
+                    for (int j = 1; j <= i; j++){
+                        System.out.print("*" + " ");
+                    }
+                    System.out.println("*" + " ");
+                }
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("No choice!");
+        }
+    }
+}
