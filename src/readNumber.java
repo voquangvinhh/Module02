3d0fb189a42a0b8077feb8729b26284987ee6d46
quangvinh18 <voquangvinh.dhqb@gmail.com>
2022-11-09 08:49:25 +0700

    add findValue.java file

--- src/readNumber.java
+++ src/readNumber.java
@@ -3,115 +3,123 @@ import java.util.Scanner;
 public class readNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int chuc;
-        int donvi;
-        int tram;
+        int hundress;
+        int dozens;
+        int unit;
+        int x = 0;
         System.out.print("nhap so hang tram: ");
-        tram = sc.nextInt();
+        hundress = sc.nextInt();
         System.out.print("Nhap so hang chuc: ");
-        chuc = sc.nextInt();
+        dozens = sc.nextInt();
         System.out.print("Nhap so hang don vi: ");
-        donvi = sc.nextInt();
-        String readNumber;
-
-            switch (tram) {
+        unit = sc.nextInt();
+        String readOneNumber;
+        String readTwoNumber;
+        String readThreeNumber;
+            switch (unit) {
+                case 0:
+                     readOneNumber = "Zero";
+                     break;
                 case 1:
-                    readNumber = "mot tram";
+                    readOneNumber = "One";
                     break;
                 case 2:
-                    readNumber = "hai tram";
+                    readOneNumber = "Two";
                     break;
                 case 3:
-                    readNumber = "ba tram";
+                    readOneNumber = "Three";
                     break;
                 case 4:
-                    readNumber = "bon tram";
+                    readOneNumber = "Four";
                     break;
                 case 5:
-                    readNumber = "nam tram";
+                    readOneNumber = "Fine";
                     break;
                 case 6:
-                    readNumber = "sau tram";
+                    readOneNumber = "Six";
                     break;
                 case 7:
-                    readNumber = "bay tram";
+                    readOneNumber = "Seven";
                     break;
                 case 8:
-                    readNumber = "tam tram";
+                    readOneNumber = "Eight";
                     break;
                 case 9:
-                    readNumber = "chin tram";
+                    readOneNumber = "Nine";
                     break;
                 default:
-                    readNumber = "";
+                    readOneNumber = "";
             }
+            if (dozens <= 19){
+                switch (dozens) {
+                    case 10:
+                        readTwoNumber = "ten";
+                        break;
+                    case 11:
+                        readTwoNumber = "eleven";
+                        break;
+                    case 12:
+                        readTwoNumber = "twelve";
+                        break;
+                    case 13:
+                        readTwoNumber = "thirteen";
+                        break;
+                    case 14:
+                        readTwoNumber = "fourteen";
+                        break;
+                    case 15:
+                        readTwoNumber = "fifteen";
+                        break;
+                    case 16:
+                        readTwoNumber = "sixteen";
+                        break;
+                    case 17:
+                        readTwoNumber = "seventeen";
+                        break;
+                    case 18:
+                        readTwoNumber = "eighteen";
+                        break;
+                    case 19:
+                        readTwoNumber = "nineteen";
+                        break;
+                    default:
+                        readTwoNumber = "";
+                }
+            if(dozens >= 20){
 
-            switch (chuc){
-                case 1:
-                    readNumber = "muoi";
-                    break;
-                case 2:
-                    readNumber = "hai tram";
-                    break;
-                case 3:
-                    readNumber = "ba tram";
-                    break;
-                case 4:
-                    readNumber = "bon tram";
-                    break;
-                case 5:
-                    readNumber = "nam tram";
-                    break;
-                case 6:
-                    readNumber = "sau tram";
-                    break;
-                case 7:
-                    readNumber = "bay tram";
-                    break;
-                case 8:
-                    readNumber = "tam tram";
-                    break;
-                case 9:
-                    readNumber = "chin tram";
-                    break;
-                default:
-                    readNumber = "";
+                switch (dozens){
+                    case 2:
+                        readTwoNumber = "Twenty";
+                        break;
+                    case 3:
+                        readTwoNumber = "Thirty";
+                        break;
+                    case 4:
+                        readTwoNumber = "Fourty";
+                        break;
+                    case 5:
+                        readTwoNumber = "Finety";
+                        break;
+                    case 6:
+                        readTwoNumber = "Sixty";
+                        break;
+                    case 7:
+                        readTwoNumber = "Seventy";
+                        break;
+                    case 8:
+                        readTwoNumber = "Eighty";
+                        break;
+                    case 9:
+                        readTwoNumber = "Ninety";
+                        break;
+                    default:
+                        readTwoNumber  = "";
+                }
             }
-
-        switch (donvi){
-            case 1:
-                readNumber = "mot";
-                break;
-            case 2:
-                readNumber = "hai";
-                break;
-            case 3:
-                readNumber = "ba";
-                break;
-            case 4:
-                readNumber = "bon";
-                break;
-            case 5:
-                readNumber = "nam";
-                break;
-            case 6:
-                readNumber = "sau";
-                break;
-            case 7:
-                readNumber = "bay";
-                break;
-            case 8:
-                readNumber = "tam";
-                break;
-            case 9:
-                readNumber = "chin";
-                break;
-            default:
-                readNumber = "";
         }
 
-        if (readNumber != "")
-        System.out.printf("The Number " + " 5read " + readNumber);
+        if (readOneNumber != "")
+        System.out.printf("The Number " + " 5read " );
         else System.out.printf("Out of ability!");
     }
 }
